2af2f40f72b0f3e325b455675fd9937ab18083a7
Lisa Chinn <lisamchinn@gmail.com>
2019-07-24 17:43:56 -0300

    added routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+EVENTS = [ {
+  title: ,
+  description: ,
+  address: ,
+  date: ,
+  start_time: ,
+  end_time: ,
+  price: ,
+  photo: ,
+  host: ,
+  host_company: ,
+}
+]

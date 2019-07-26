7e8290a1510b51e2ca96c294e60d84bdfe9edb47
Lisa Chinn <lisamchinn@gmail.com>
2019-07-26 19:45:59 -0300

    started working on survey questions controller and views

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,8 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    @question_mentee = Survey.where(role: "mentee").first.questions.first
+    @question_mentor = Survey.where(role: "mentor").first.questions.first
+
   end
 end

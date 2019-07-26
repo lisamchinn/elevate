8cc103426a9aafc28e56207b0715c5f650232450
Lisa Chinn <lisamchinn@gmail.com>
2019-07-26 11:08:35 -0300

    tried to add move.js but still not working

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -14,3 +14,35 @@ const initUpdateNavbarOnScroll = () => {
 }
 
 export { initUpdateNavbarOnScroll };
+
+const moveLogo = () => {
+  const logo = document.querySelector('.logo');
+  const navbar = document.querySelector('.navbar-ellevate');
+  const navTop = navbar.offsetTop
+
+  if (navbar) {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY >= navTop) {
+        move('.logo')
+          .to(500, 200)
+          .rotate(180)
+          .scale(.5)
+          .set('background-color', '#888')
+          .set('border-color', 'black')
+          .duration('2s')
+          .skew(50, -10)
+          .then()
+            .set('opacity', 0)
+            .duration('0.3s')
+            .scale(0.1)
+            .pop()
+          .end();
+      };
+    });
+  }
+}
+
+export { moveLogo };
+
+
+

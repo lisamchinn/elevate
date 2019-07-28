c1076cf44e4fbaf4a66d36624bcf2e892992f302
Alberta Lowe <albertalowe93@gmail.com>
2019-07-28 20:30:24 -0300

    Added fonts, added 4 new seeds

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,8 @@ module Elevate
         end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
+    config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers

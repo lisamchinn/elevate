817d27aea4a319aab73bb2911290e87697a6be3d
Alberta Lowe <albertalowe93@gmail.com>
2019-07-29 10:46:50 -0300

    Added default donation amount

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,3 +7,6 @@ initUpdateNavbarOnScroll();
 
 import { moveLogo } from '../components/navbar';
 moveLogo();
+
+import { initDefaultDonationValue } from '../components/init_default_donation_amount';
+initDefaultDonationValue();

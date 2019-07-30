3bb6d48207316042c5927e64f2a161083748af5c
Leandro Iamele <leandroiamele@gmail.com>
2019-07-30 16:47:18 -0300

    NavBar links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def question_mentee
+    Survey.find_by(role: "mentee").survey_questions.first.question
+  end
+
+  def question_mentee_link(class_name=nil)
+    link_to "Take the Survey", question_path(question_mentee), class: class_name
+  end
 end

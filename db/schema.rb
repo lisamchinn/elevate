3af478810c86a007977cd0106976e808dfbe6e71
Lisa Chinn <lisamchinn@gmail.com>
2019-09-04 14:40:52 +0200

    added 2 columns to event table -- host photo, city

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_31_132034) do
+ActiveRecord::Schema.define(version: 2019_09_04_123804) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,8 @@ ActiveRecord::Schema.define(version: 2019_07_31_132034) do
     t.string "host_company"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "host_photo"
+    t.string "city"
   end
 
   create_table "forums", force: :cascade do |t|
